659e436b03ecc93a250d0773357b5200a9b822a1
Indrajaiswal <95992428+Indrajaiswal@users.noreply.github.com>
2022-03-11 20:42:08 +0545

    commit

---
+++ footer.php
@@ -0,0 +1,32 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>Quick Links</h3>
+         <a href="home.php">Home</a>
+         <a href="about.php">About</a>
+         <a href="shop.php">Shop</a>
+         <a href="contact.php">Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra Links</h3>
+         <a href="login.php">Login</a>
+         <a href="register.php">Register</a>
+         <a href="cart.php">Cart</a>
+         <a href="orders.php">Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Info</h3>
+         <p> <i class="fas fa-phone"></i> +977 9889898989 </p>
+         <p> <i class="fas fa-phone"></i> +977 9898989898 </p>
+         <p> <i class="fas fa-envelope"></i> Kishan@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Kalanki, Kathmandu - 01-4256598 </p>
+      </div>
+   </div>
+
+   <p class="credit"> &copy; Copyright  @ <?php echo date('Y'); ?> By <span>Kishan Jaishwal</span> </p>
+
+</section>
